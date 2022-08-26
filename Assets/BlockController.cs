14a3636e985882b9b88c16b49ef803a20e61fdc1
umenokoudai <umenokoudai0805@gmail.com>
2022-08-26 21:15:12 +0900

    ステージ作成中 プレイヤースクリプト修正

---
+++ Assets/BlockController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BlockController : MonoBehaviour
+{
+    /// <summary>プレイヤーの場所を取得</summary>
+    [SerializeField] Transform _player;
+    /// <summary>Rigidbody2Dを格納する場所</summary>
+    Rigidbody2D _rb;
+    /// <summary>ブロックの静止と移動の判定</summary>
+    bool _isBlockMove;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    //ブロックを移動させるためのメソッド
+    public void Move(int MoveSpeed)
+    {
+        //もう1度実行されると静止と移動の判定が逆になる
+        _isBlockMove = !_isBlockMove;
+        //_isBlockMoveがtrueの場合はプレイヤーに向かって移動する
+        if (_isBlockMove)
+        {
+            //bodyTypeがDynamicになる
+            _rb.bodyType = RigidbodyType2D.Dynamic;
+            //プレイヤーに向かって移動する
+            _rb.velocity = (_player.position - transform.position).normalized * MoveSpeed;
+        }
+        //_isBlockMoveがfalseの場合は移動が静止する
+        else
+        {
+            //bodyTypeがKinematicになる(プレイヤーが当たっても動かなくなる)
+            _rb.bodyType = RigidbodyType2D.Kinematic;
+            //移動の力(ベクトル)がゼロになる
+            _rb.velocity = Vector2.zero;
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    /// <summary>左右移動の力/</summary>
+    [SerializeField] int _moveSpeed;
+    /// <summary>ジャンプの力/</summary>
+    [SerializeField] int _jumpPower;
+    /// <summary>ブロックを左に動かす力/</summary>
+    [SerializeField] int _blockMoveSpeedLeft;
+    /// <summary>ブロックを右に動かす力/</summary>
+    [SerializeField] int _blockMoveSpeedRigit;
+    /// <summary>Raycastでオブジェクトを判定するレイヤー/</summary>
+    [SerializeField] LayerMask _block;
+    /// <summary>Rigidbody2Dの格納場所/</summary>
+    Rigidbody2D _rb;
+    /// <summary>ジャンプ判定の変数/</summary>
+    bool _isGround;
+    /// <summary>左右移動の入力を格納する変数/</summary>
+    float _x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Rigidbody2Dを格納
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        //キャラの移動(左右移動)　　　　　　　　　　　　　　　　　　　　　　　　　　　　
+        _rb.velocity = new Vector2(_x * _moveSpeed, _rb.velocity.y);
+        //キャラの移動(ジャンプ)
+        if(Input.GetButtonDown("Jump") && _isGround)
+        {
+            _rb.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
+        }
+    }
+    void Update()
+    {
+        //左右の入力を変数に格納
+        _x = Input.GetAxisRaw("Horizontal");
+        //プレイヤーを入力方向に向ける
+        FlipX(_x);
+        //Rayを飛ばす為のマウスポジションを取得
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //カメラの位置からマウスの位置までRayを飛ばす
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+        Debug.DrawLine(ray.origin, ray.direction, Color.red);
+        //移動可能のブロックを動かす(プレイヤーの方向に移動する)
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //Rayが当たったオブジェクトのtagがBlockだったら処理を実行
+            if (hit.collider.gameObject.tag == "Block")
+            {
+                //変数BlockにRayが当たったオブジェクトを代入
+                GameObject Block = hit.transform.gameObject;
+                //Blockに入ったオブジェクトに入っているBlockControllerを変数に代入
+                BlockController bloackMove = Block.GetComponent<BlockController>();
+                //BlockControllerにあるMoveメソッドを実行
+                bloackMove.Move(_blockMoveSpeedLeft);
+            }
+        }
+        //移動可能のブロックを動かす(プレイヤーと逆方向に移動する)
+        if (Input.GetButtonDown("Fire2"))
+        {
+            //Rayが当たったオブジェクトのtagがBlockだったら処理を実行
+            if (hit.collider.gameObject.tag == "Block")
+            {
+                //変数BlockにRayが当たったオブジェクトを代入
+                GameObject Block = hit.transform.gameObject;
+                //Blockに入ったオブジェクトに入っているBlockControllerを変数に代入
+                BlockController bloackMove = Block.GetComponent<BlockController>();
+                //BlockControllerにあるMoveメソッドを実行
+                bloackMove.Move(_blockMoveSpeedRigit);
+            }
+        }
+    }
+    //プレイヤーを入力方向に向けるメソッド
+    void FlipX(float X)
+    {
+        if(X > 0)
+        {
+            transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
+        else if(X < 0)
+        {
+            transform.localScale = new Vector2(-1 * Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
+    }
+    //接地判定
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ground" || collision.tag == "Block")
+        {
+            _isGround = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Ground" || collision.tag == "Block")
+        {
+            _isGround = false;
+        }
+    }
+}
