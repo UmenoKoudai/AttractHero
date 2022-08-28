9ab3f7c0bb965edb1594936b42380ddd3a8c128c
umenokoudai <umenokoudai0805@gmail.com>
2022-08-28 23:22:05 +0900

    タイトル画面とゲームクリア画面追加

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,12 +7,14 @@ public class GameManager : MonoBehaviour
 {
     /// <summary>Timer表示のテキストを格納する変数</summary>
     [SerializeField] Text _timerText;
+    [SerializeField] Text _resultTimer;
     /// <summary>SetActiveで非表示にするオブジェクトを格納する変数</summary>
     [SerializeField] GameObject _close;
     /// <summary>SetActiveで表示させるオブジェクトを格納する変数</summary>
     [SerializeField] GameObject _open;
     /// <summary>Playerスクリプトを格納する変数</summary>
     Player _playerScript;
+    ScneMove _scneMove;
     /// <summary>ゲーム終了したらtrueになって動作を止める</summary>
     bool _isFinish;
     /// <summary>_close変数をプロパティ化</summary>
@@ -43,13 +45,21 @@ public class GameManager : MonoBehaviour
                 _timerText.text = $"Time:{_timer.ToString("f2")}";
             }
         }
+        else
+        {
+            if(_resultTimer)
+            {
+                _resultTimer.text = $"Time{_timer.ToString("f2")}";
+            }
+        }
     }
     //ScneMoveスクリプトのSetActiveメソッドを実行
     public  void GameManagerSetActive()
     {
         if(Close || Open)
         {
-            ScneMove.SetActive(Close, Open);
+            _scneMove = GameObject.FindObjectOfType<ScneMove>();
+            _scneMove.SetActive(Close, Open);
         }
     }
     //ゲーム終了時に実行される
--- Assets/ScneMove.cs
+++ Assets/ScneMove.cs
@@ -5,14 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class ScneMove : MonoBehaviour
 {
+    [SerializeField] GameObject _close;
+    [SerializeField] GameObject _open;
     //シーンを移動させるソッド
     public　static void SceneMove(string SceneName)
     {
         SceneManager.LoadScene(SceneName);
     }
-    public static void SetActive(GameObject Close, GameObject Open)
+    public void SetActive(GameObject Close, GameObject Open)
     {
         Close.gameObject.SetActive(false);
         Open.gameObject.SetActive(true);
+    }
+    public void SetActiveButton()
+    {
+        _close.gameObject.SetActive(false);
+        _open.gameObject.SetActive(true);
     }
 }
