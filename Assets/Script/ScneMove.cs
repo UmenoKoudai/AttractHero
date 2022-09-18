7778f32e4bfaf2e1f398bbf89d83e876d85efbd0
umenokoudai <umenokoudai0805@gmail.com>
2022-09-19 01:40:44 +0900

    ヘルプ作成中

--- Assets/Script/ScneMove.cs
+++ Assets/Script/ScneMove.cs
@@ -6,7 +6,9 @@ using UnityEngine.SceneManagement;
 public class ScneMove : MonoBehaviour
 {
     [SerializeField] GameObject _close;
+    [SerializeField] GameObject _close2;
     [SerializeField] GameObject _open;
+    [SerializeField] GameObject _open2;
     //シーンを移動させるソッド
     public　static void SceneMove(string SceneName)
     {
@@ -21,5 +23,12 @@ public class ScneMove : MonoBehaviour
     {
         _close.gameObject.SetActive(false);
         _open.gameObject.SetActive(true);
+    }
+    public void SetActiveButton4()
+    {
+        _close.gameObject.SetActive(false);
+        _open.gameObject.SetActive(true);
+        _close2.gameObject.SetActive(false);
+        _open2.gameObject.SetActive(true);
     }
 }
